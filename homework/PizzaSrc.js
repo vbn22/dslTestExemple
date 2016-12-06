b89c8c5b71ac929556afbc12181fad3fb4fb5444
michael <v.b.n@mail.ru>
2016-12-06 12:08:40 +0300

    fix

--- homework/PizzaSrc.js
+++ homework/PizzaSrc.js
@@ -3,6 +3,20 @@
  */
 'use strict';
 
+
+export class Pizzeria {
+    constructor(){
+        this.orders = {};
+    }
+
+    add_order(user_name,pizza) {
+        this.orders[user_name] = pizza;
+        return this.orders[user_name];
+    }
+
+};
+
+
 export class Pizza {
     constructor(filling, olives, dough, price){
         this.filling = filling;
@@ -11,26 +25,57 @@ export class Pizza {
         this.price = price;
         this.cake = false;
     }
+};
 
-    buy(incomin,count){
-        if (incomin > this.price*count){
-            let delivery = incomin - this.price * count
-            if (count >= 3){
-                this.cake = true;
-            }
-            return delivery;
-        } else {
-            throw "Not enough money";
-        }
+
+export class Customer {
+    constructor(user_name,money){
+        this.user_name = user_name;
+        this.money = money;
+        this.pizza = null;
     }
 
+    wantsPizza(pizza){
+        this.pizza = pizza;
+        return this.pizza;
+    }
 
+    placeOrder(pizzeria){
+        return pizzeria.add_order(this.user_name,this.pizza);
+        // if (incomin > this.price*count){
+        //     let delivery = incomin - this.price * count
+        //     if (count >= 3){
+        //         this.cake = true;
+        //     }
+        //     return delivery;
+        // } else {
+        //     throw "Not enough money";
+        // }
+    }
 
 };
 
 
-export function NewPizza () {
+export function NewCustomer(){
+    var state = {}
 
+    this.withName = function(name){
+        state.name = name;
+        return this;
+    }
+
+    this.withMoney = function(money){
+        state.money = money;
+        return this;
+    }
+
+    this.build = function(){
+        return new Customer(state.name,state.money);
+    }
+}
+
+export function NewPizza () {
+    
     var state = {
         olives:false,
     };
@@ -55,7 +100,7 @@ export function NewPizza () {
     }
 
     this.build = function () {
-        console.log(state);
+        //console.log(state);
         return new Pizza(state.filling, state.olives, state.dough, state.price);
     }
 };
--- homework/PizzaTests.js
+++ homework/PizzaTests.js
@@ -2,43 +2,50 @@
  * Created by michael on 07.11.16.
  */
 import assert from 'assert'
-import { NewPizza,Pizza } from '../homework/PizzaSrc.js'
+import { NewPizza,Pizza,NewCustomer,Pizzeria } from '../homework/PizzaSrc.js'
 
 suite('when i want to some pizza', function () {
     let pizza;
+    let pizzeria;
+    let customer;
     let price = 25;
     setup(function(){
-         pizza = new NewPizza()
-        .withFilling("chiken")
-        .withOlivies()
-        .withDough('thin')
-        .withPrice(price)
-        .build();
+        customer = new NewCustomer()
+            .withName('Michael')
+            .withMoney(50)
+            .build();
+
+        pizza = new NewPizza()
+            .withFilling("chiken")
+            .withOlivies()
+            .withDough('thin')
+            .withPrice(price)
+            .build();
+
+        customer.wantsPizza(pizza);
+        pizzeria = new Pizzeria();
     })
 
+
+    // Тест метода отправки заявки заказа в пиццерию
+    test('Customer can create order with pizza', function () {
+        let order_pizza = customer.placeOrder(pizzeria);
+        assert.equal(pizza, order_pizza)
+    });
+
+    // Тесты заказа в пицерии
     test('Filling pizza is chiken', function () {
-        assert.equal('chiken', pizza.filling);
+        let order_pizza = customer.placeOrder(pizzeria);
+        assert.equal('chiken', order_pizza.filling);
     });
 
     test('Pizza with Olivies', function () {
-        assert.equal(true, pizza.olives);
+        let order_pizza = customer.placeOrder(pizzeria);
+        assert.equal(true, order_pizza.olives);
     });
 
     test('Dough pizza is thin', function () {
-        assert.equal('thin', pizza.dough);
+        let order_pizza = customer.placeOrder(pizzeria);
+        assert.equal('thin', order_pizza.dough);
     });
-
-    test('i can buy pizza with 30 money', function () {
-        let delivery = pizza.buy(30,1);
-        assert.equal(30-price, delivery);
-    })
-
-    test('i can NOT buy pizza with 20 money', function () {
-        assert.throws(function(){pizza.buy(20,1)}, /Not enough money/);
-    })
-
-    test('get cake if i buy 3 pizzas', function () {
-        let delivery = pizza.buy(100,3);
-        assert.equal(true,pizza.cake);
-    })
 });
